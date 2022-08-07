72866458635a8ffa643c6eaae2d12503ad7f8b15
Guollove <2091812893@qq.com>
2022-08-07 23:52:16 +0800

    列表数据请求与渲染 日期与按钮

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,11 +1,42 @@
 import React, { useEffect, useState } from "react";
+import { List, Button, Skeleton, Pagination } from "antd";
+import { ArticleListApi } from "../request/api";
+import moment from "moment";
 import "./less/List.less";
-import { List, Skeleton } from "antd";
 
 export default function Lists() {
   const [list, setList] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [current, setCorrent] = useState(1);
+  const [pageSize, setPageSize] = useState(10);
+
+  //请求封装
+  const getList = (num) => {
+    ArticleListApi({
+      num,
+      count: pageSize,
+    }).then((res) => {
+      if (res.errCode === 0) {
+        let { arr, total, num, count } = res.data;
+        setList(arr);
+        setTotal(total);
+        setCorrent(num);
+        setPageSize(count);
+      }
+    });
+  };
+
+  //请求列表数据
+  useEffect(() => {
+    getList(current);
+  }, []);
+
+  const onChange = (pages) => {
+    getList(pages);
+  };
+
   return (
-    <div className="list_table">
+    <div className="list_table" style={{ padding: "20px" }}>
       <List
         className="demo-loadmore-list"
         itemLayout="horizontal"
@@ -13,16 +44,30 @@ export default function Lists() {
         renderItem={(item) => (
           <List.Item
             actions={[
-              <a key="list-loadmore-edit">edit</a>,
-              <a key="list-loadmore-more">more</a>,
+              <Button type="primary" onClick={() => console.log(item.id)}>
+                编辑
+              </Button>,
+              <Button type="danger" onClick={() => console.log(item.id)}>
+                删除
+              </Button>,
             ]}
           >
-            <Skeleton avatar title={false}>
-              <List.Item.Meta title={<a href="!#">bt</a>} description="fbt" />
-              <div>日期</div>
+            <Skeleton loading={false} title={false}>
+              <List.Item.Meta
+                title={<a href="!#">{item.title}</a>}
+                description={item.subTitle}
+              />
+              <div>{moment(item.date).format("YYYY-MM-DD hh:mm:ss")}</div>
             </Skeleton>
           </List.Item>
         )}
+      />
+      <Pagination
+        style={{ float: "right", marginTop: "20px" }}
+        onChange={onChange}
+        total={total}
+        current={current}
+        pageSize={pageSize}
       />
     </div>
   );
